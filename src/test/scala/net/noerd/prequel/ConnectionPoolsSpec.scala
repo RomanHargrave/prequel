6d6e7e99134afb8f085c57939412b9b222dce54d
Giovanni Caruso <vanni.caruso@gmail.com>
2013-06-16 17:39:09 +0200

    Now it can use a jndi connection or an external connection or a dbcp connection pool.

--- src/test/scala/net/noerd/prequel/ConnectionPoolsSpec.scala
+++ src/test/scala/net/noerd/prequel/ConnectionPoolsSpec.scala
@@ -1,54 +1,54 @@
 package net.noerd.prequel
 
-import java.sql.SQLException
 
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.BeforeAndAfterEach
 
 class ConnectionPoolsSpec extends FunSpec with ShouldMatchers with BeforeAndAfterEach {
-    
-    val config1 = DatabaseConfig( 
-        driver = "org.hsqldb.jdbc.JDBCDriver",
-        jdbcURL = "jdbc:hsqldb:mem:config1"
-    )
-    val config1Copy = DatabaseConfig( 
-        driver = "org.hsqldb.jdbc.JDBCDriver",
-        jdbcURL = "jdbc:hsqldb:mem:config1"
-    )
-    val config2 = DatabaseConfig( 
-        driver = "org.hsqldb.jdbc.JDBCDriver",
-        jdbcURL = "jdbc:hsqldb:mem:config2"
-    )
 
-    override def beforeEach() = {
-        ConnectionPools.reset()
+  val config1 = DatabaseConfig(
+    driver = "org.hsqldb.jdbc.JDBCDriver",
+    jdbcURL = "jdbc:hsqldb:mem:config1"
+  )
+  val config1Copy = DatabaseConfig(
+    driver = "org.hsqldb.jdbc.JDBCDriver",
+    jdbcURL = "jdbc:hsqldb:mem:config1"
+  )
+  val config2 = DatabaseConfig(
+    driver = "org.hsqldb.jdbc.JDBCDriver",
+    jdbcURL = "jdbc:hsqldb:mem:config2"
+  )
+
+  override def beforeEach() = {
+    ConnectionPools.reset()
+  }
+
+  describe("ConnectionPools") {
+
+    describe("getOrCreatePool") {
+
+      it("should create a new pool for each unique Configuration") {
+
+        ConnectionPools.getOrCreatePool(config1)
+        ConnectionPools.nbrOfPools should be(1)
+
+        ConnectionPools.getOrCreatePool(config2)
+        ConnectionPools.nbrOfPools should be(2)
+      }
+
+      it("should reuse an existing pool if the configuration is the same") {
+
+        ConnectionPools.getOrCreatePool(config1)
+        ConnectionPools.nbrOfPools should be(1)
+
+        ConnectionPools.getOrCreatePool(config1)
+        ConnectionPools.nbrOfPools should be(1)
+
+        ConnectionPools.getOrCreatePool(config1Copy)
+        ConnectionPools.nbrOfPools should be(1)
+      }
     }
-    describe( "ConnectionPools" ) {
-        
-        describe( "getOrCreatePool" ) {
-        
-            it( "should create a new pool for each unique Configuration" ) {
-            
-                ConnectionPools.getOrCreatePool( config1 )
-                ConnectionPools.nbrOfPools should be (1)
-
-                ConnectionPools.getOrCreatePool( config2 )
-                ConnectionPools.nbrOfPools should be (2)
-            }
-            
-            it( "should reuse an existing pool if the configuration is the same" ) {
-                
-                ConnectionPools.getOrCreatePool( config1 )
-                ConnectionPools.nbrOfPools should be (1)
-
-                ConnectionPools.getOrCreatePool( config1 )
-                ConnectionPools.nbrOfPools should be (1)
-
-                ConnectionPools.getOrCreatePool( config1Copy )
-                ConnectionPools.nbrOfPools should be (1)
-            }
-        }
-    }
+  }
 
 }
--- src/test/scala/net/noerd/prequel/FormattablesSpec.scala
+++ src/test/scala/net/noerd/prequel/FormattablesSpec.scala
@@ -4,85 +4,85 @@ import org.joda.time.Duration
 
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.BeforeAndAfterEach
 import java.util.Locale
 
 class FormattablesSpec extends FunSpec with ShouldMatchers {
 
-    val formatter = SQLFormatter.HSQLDBSQLFormatter    
-    val formattables = List(
-        ( "StringFormattable should escape foo", 
-            StringFormattable( "foo"), "'foo'" 
-        ),
-        ( "LongFormattable should escape 1234", 
-            LongFormattable( 1234L ), "1234" 
-        ),
-        ( "BooleanFormattable should escape true", 
-            BooleanFormattable( true ), "true" 
-        ),
-        ( "BooleanFormattable should escape false", 
-            BooleanFormattable( false ), "false" 
-        ),
-        ( "FloatFormattable should escape 1.500000", 
-            FloatFormattable( 1.500000F ), "1.500000" 
-        ),        
-        ( "DoubleFormattable should escape 1.500000", 
-            DoubleFormattable( 1.500000F ), "1.500000" 
-        ),        
-        ( "DateTimeFormattable should escape 2010-03-13 13:00:00.0000", 
-            DateTimeFormattable( formatter.timeStampFormatter.parseDateTime( "2010-03-13 13:00:00.0000" ) ), 
-            "'2010-03-13 13:00:00.0000'"
-        ),
-        ( "DurationFormattable should escape an Duration object", 
-            DurationFormattable( Duration.standardHours( 2 ) ), "7200000"
-        ),
-        ( "NullComparable should escape defined formattable", 
-            NullComparable( Some( StringFormattable( "foo" ) ) ), "='foo'"
-        ),
-        ( "NullComparable should escape undefined formattable", 
-            NullComparable( None ), "is null"
-        ),
-        ( "Nullable should escape defined formattables", 
-            Nullable( Some( StringFormattable( "foo" ) ) ), "'foo'" 
-        ),        
-        ( "Nullable should escape undefined formattables", 
-            Nullable( None ), "null" 
-        ),
-        ( "Identifier should leave string as they are", 
-            Identifier( "this.is.a.stri'ng" ), "this.is.a.stri'ng"
-        ),
-        ( "BinaryFormattable should escape and convert defined formattable to hexadecimal format",
-            BinaryFormattable( "Lorem Ipsum".getBytes ),
-            "'4C6F72656D20497073756D'"
-        )
-    )
+  val formatter = SQLFormatter.HSQLDBSQLFormatter
+  val formattables = List(
+    ("StringFormattable should escape foo",
+      StringFormattable("foo"), "'foo'"
+      ),
+    ("LongFormattable should escape 1234",
+      LongFormattable(1234L), "1234"
+      ),
+    ("BooleanFormattable should escape true",
+      BooleanFormattable(true), "true"
+      ),
+    ("BooleanFormattable should escape false",
+      BooleanFormattable(false), "false"
+      ),
+    ("FloatFormattable should escape 1.500000",
+      FloatFormattable(1.500000F), "1.500000"
+      ),
+    ("DoubleFormattable should escape 1.500000",
+      DoubleFormattable(1.500000F), "1.500000"
+      ),
+    ("DateTimeFormattable should escape 2010-03-13 13:00:00.0000",
+      DateTimeFormattable(formatter.timeStampFormatter.parseDateTime("2010-03-13 13:00:00.0000")),
+      "'2010-03-13 13:00:00.0000'"
+      ),
+    ("DurationFormattable should escape an Duration object",
+      DurationFormattable(Duration.standardHours(2)), "7200000"
+      ),
+    ("NullComparable should escape defined formattable",
+      NullComparable(Some(StringFormattable("foo"))), "='foo'"
+      ),
+    ("NullComparable should escape undefined formattable",
+      NullComparable(None), "is null"
+      ),
+    ("Nullable should escape defined formattables",
+      Nullable(Some(StringFormattable("foo"))), "'foo'"
+      ),
+    ("Nullable should escape undefined formattables",
+      Nullable(None), "null"
+      ),
+    ("Identifier should leave string as they are",
+      Identifier("this.is.a.stri'ng"), "this.is.a.stri'ng"
+      ),
+    ("BinaryFormattable should escape and convert defined formattable to hexadecimal format",
+      BinaryFormattable("Lorem Ipsum".getBytes),
+      "'4C6F72656D20497073756D'"
+      )
+  )
 
-    describe( "Formattables" ) {
-        
-        formattables.foreach { case( description, formattable, expected ) =>
-            it( description ) {
-                formattable.escaped( formatter ) should equal ( expected )
-            }            
+  describe("Formattables") {
+
+    formattables.foreach {
+      case (description, formattable, expected) =>
+        it(description) {
+          formattable.escaped(formatter) should equal(expected)
         }
     }
+  }
 
-    describe( "FloatFormattable" ) {
-        it( "should escape 1.5 correctly even with non-US locales" ) {
-            val oldDefaultLocale = Locale.getDefault
+  describe("FloatFormattable") {
+    it("should escape 1.5 correctly even with non-US locales") {
+      val oldDefaultLocale = Locale.getDefault
 
-            Locale.setDefault(Locale.GERMANY)
-            FloatFormattable(1.500000f).escaped( formatter ) should equal ( "1.500000" )
-            Locale.setDefault(oldDefaultLocale)
-        }
+      Locale.setDefault(Locale.GERMANY)
+      FloatFormattable(1.500000f).escaped(formatter) should equal("1.500000")
+      Locale.setDefault(oldDefaultLocale)
     }
+  }
 
-    describe( "DoubleFormattable" ) {
-        it( "should escape 1.5 correctly even with non-US locales" ) {
-            val oldDefaultLocale = Locale.getDefault
+  describe("DoubleFormattable") {
+    it("should escape 1.5 correctly even with non-US locales") {
+      val oldDefaultLocale = Locale.getDefault
 
-            Locale.setDefault(Locale.GERMANY)
-            DoubleFormattable(1.500000f).escaped( formatter ) should equal ( "1.500000" )
-            Locale.setDefault(oldDefaultLocale)
-        }
+      Locale.setDefault(Locale.GERMANY)
+      DoubleFormattable(1.500000f).escaped(formatter) should equal("1.500000")
+      Locale.setDefault(oldDefaultLocale)
     }
+  }
 }
